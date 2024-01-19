45499806e1e242a1cbeac3666c824eb1ef9af4b6
dmaestre <dmaestre@bancocaminos.es>
2024-01-19 15:40:39 +0100

    Test new sitemap filtering config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,11 +11,26 @@ export default defineConfig({
     tailwind(),
     mdx(),
     sitemap({
-      filter: (page) =>
-        page !==
-          "https://webllope.es/en/blog/importancia-tener-pagina-web/" &&
-        page !==
+      ffilter: (page) => {
+        // Exclude specific URLs from modification
+        const excludedUrls = [
+          "https://webllope.es/en/blog/importancia-tener-pagina-web/",
           "https://webllope.es/es/blog/is-it-important-to-have-a-website/",
+        ];
+
+        if (excludedUrls.includes(page)) {
+          return page;
+        }
+
+        // Modify URLs to include "www" if the hostname is not already "www.webllope.es"
+        const modifiedUrl = new URL(page);
+        if (modifiedUrl.hostname === "webllope.es") {
+          modifiedUrl.hostname = "www.webllope.es";
+        }
+
+        // Construct the modified URL
+        return modifiedUrl.toString();
+      },
     }),
   ],
   adapter: vercel({
