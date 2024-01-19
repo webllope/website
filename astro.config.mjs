667e9640bd5fb279fed6fecfc241a5125022a3fd
dmaestre <dmaestre@bancocaminos.es>
2024-01-19 15:51:43 +0100

    se viene avalancha de tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,29 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/static";
 
+function filterPages(page) {
+ return page !== "https://webllope.es/en/blog/importancia-tener-pagina-web/" && page !== "https://webllope.es/es/blog/is-it-important-to-have-a-website/";
+}
+// {
+//         // Exclude specific URLs from modification
+//         const excludedUrls = [
+//           "https://webllope.es/en/blog/importancia-tener-pagina-web/",
+//           "https://webllope.es/es/blog/is-it-important-to-have-a-website/",
+//         ];
+
+//         if (excludedUrls.includes(page)) {
+//           return page;
+//         }
+
+//         // Modify URLs to include "www" if the hostname is not already "www.webllope.es"
+//         const modifiedUrl = new URL(page);
+//         if (modifiedUrl.hostname === "webllope.es") {
+//           modifiedUrl.hostname = "www.webllope.es";
+//         }
+
+//         // Construct the modified URL
+//         return modifiedUrl.toString();
+//       },
 // https://astro.build/config
 export default defineConfig({
   site: "https://webllope.es",
@@ -11,26 +34,7 @@ export default defineConfig({
     tailwind(),
     mdx(),
     sitemap({
-      filter: (page) => {
-        // Exclude specific URLs from modification
-        const excludedUrls = [
-          "https://webllope.es/en/blog/importancia-tener-pagina-web/",
-          "https://webllope.es/es/blog/is-it-important-to-have-a-website/",
-        ];
-
-        if (excludedUrls.includes(page)) {
-          return page;
-        }
-
-        // Modify URLs to include "www" if the hostname is not already "www.webllope.es"
-        const modifiedUrl = new URL(page);
-        if (modifiedUrl.hostname === "webllope.es") {
-          modifiedUrl.hostname = "www.webllope.es";
-        }
-
-        // Construct the modified URL
-        return modifiedUrl.toString();
-      },
+      filter: (page) => filterPages(page),
     }),
   ],
   adapter: vercel({
