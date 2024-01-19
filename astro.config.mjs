74838b8f11de94273f5a081de144e5e03ad5a29e
dmaestre <dmaestre@bancocaminos.es>
2024-01-19 17:00:26 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,29 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/static";
 
-function filterPages(page) {
- return page !== "https://webllope.es/en/blog/importancia-tener-pagina-web/" && page !== "https://webllope.es/es/blog/is-it-important-to-have-a-website/";
+function filterOutPages(page) {
+  return (
+    page !== "https://webllope.es/en/blog/importancia-tener-pagina-web/" &&
+    page !== "https://webllope.es/es/blog/is-it-important-to-have-a-website/"
+  );
 }
-// {
-//         // Exclude specific URLs from modification
-//         const excludedUrls = [
-//           "https://webllope.es/en/blog/importancia-tener-pagina-web/",
-//           "https://webllope.es/es/blog/is-it-important-to-have-a-website/",
-//         ];
-
-//         if (excludedUrls.includes(page)) {
-//           return page;
-//         }
-
-//         // Modify URLs to include "www" if the hostname is not already "www.webllope.es"
-//         const modifiedUrl = new URL(page);
-//         if (modifiedUrl.hostname === "webllope.es") {
-//           modifiedUrl.hostname = "www.webllope.es";
-//         }
-
-//         // Construct the modified URL
-//         return modifiedUrl.toString();
-//       },
 // https://astro.build/config
 export default defineConfig({
   site: "https://webllope.es",
@@ -34,7 +17,16 @@ export default defineConfig({
     tailwind(),
     mdx(),
     sitemap({
-      filter: (page) => filterPages(page),
+      filter: (page) => filterOutPages(page),
+      serialize(item) {
+        if (item.url.startsWith("https://webllope.es/")) {
+          const modifiedUrl = new URL(item.url);
+          modifiedUrl.hostname = "www.webllope.es";
+          item.url = modifiedUrl.toString();
+        }
+
+        return item;
+      },
     }),
   ],
   adapter: vercel({
