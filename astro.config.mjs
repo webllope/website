7b00846e5257ab3813d9115078f7dc29884a544b
dmaestre <dmaestre@bancocaminos.es>
2024-02-18 22:00:55 +0100

    Set up an auto email after submitting form rather than formspree

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 function filterOutPages(page) {
   return (
@@ -37,6 +37,7 @@ export default defineConfig({
       enabled: true,
     },
   }),
+  output: 'hybrid',
   i18n: {
     defaultLocale: "es",
     locales: ["en", "es"],
